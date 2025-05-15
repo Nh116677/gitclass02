c89c2d2980d811321124a354328fe4d67fc48c7e
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-05-15 14:51:38 -0400

    1st commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,10 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("This is my git project version 1");
+
     }
 }
-// done with version two
+// done with version 1
